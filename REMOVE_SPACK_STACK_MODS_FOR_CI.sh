79385e545912dd05008d75bfaca0200c8313b399
Dom Heinzeller <dom.heinzeller@icloud.com>
2025-09-02 19:55:34 -0600

    Make REMOVE_SPACK_STACK_MODS_FOR_CI.sh work on macOS

--- REMOVE_SPACK_STACK_MODS_FOR_CI.sh
+++ REMOVE_SPACK_STACK_MODS_FOR_CI.sh
@@ -2,14 +2,20 @@
 
 set -e
 
-sed -i 's/extensions:/#extensions:/g' ./etc/spack/defaults/config.yaml
-sed -i 's/- ${SPACK_STACK_DIR}/#- ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/config.yaml
+if [ "$(uname)" == "Darwin" ]; then
+  SED_PARAM="''"
+else
+  SED_PARAM=""
+fi
 
-sed -i 's/#builtin:/builtin:/g' ./etc/spack/defaults/repos.yaml
-sed -i 's/#  git: https/  git: https/g' ./etc/spack/defaults/repos.yaml
-sed -i 's/#  branch: /  branch: /g' ./etc/spack/defaults/repos.yaml
-sed -i 's/builtin: ${SPACK_STACK_DIR}/#builtin: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/repos.yaml
-sed -i 's/spack_stack: ${SPACK_STACK_DIR}/#spack_stack: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/repos.yaml
+sed -i ${SED_PARAM} 's/extensions:/#extensions:/g' ./etc/spack/defaults/config.yaml
+sed -i ${SED_PARAM} 's/- ${SPACK_STACK_DIR}/#- ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/config.yaml
+
+sed -i ${SED_PARAM} 's/#builtin:/builtin:/g' ./etc/spack/defaults/repos.yaml
+sed -i ${SED_PARAM} 's/#  git: https/  git: https/g' ./etc/spack/defaults/repos.yaml
+sed -i ${SED_PARAM} 's/#  branch: /  branch: /g' ./etc/spack/defaults/repos.yaml
+sed -i ${SED_PARAM} 's/builtin: ${SPACK_STACK_DIR}/#builtin: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/repos.yaml
+sed -i ${SED_PARAM} 's/spack_stack: ${SPACK_STACK_DIR}/#spack_stack: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/repos.yaml
 
 set +e
 
