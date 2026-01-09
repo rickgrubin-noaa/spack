2bbe02f981553b11ee428e3257289d38c000992f
Dom Heinzeller <dom.heinzeller@icloud.com>
2026-01-09 14:22:12 -0700

    Update REMOVE_SPACK_STACK_MODS_FOR_CI.sh: new location for config.yaml and repos.yaml

--- REMOVE_SPACK_STACK_MODS_FOR_CI.sh
+++ REMOVE_SPACK_STACK_MODS_FOR_CI.sh
@@ -8,14 +8,14 @@ else
   SED_PARAM=""
 fi
 
-sed -i ${SED_PARAM} 's/extensions:/#extensions:/g' ./etc/spack/defaults/config.yaml
-sed -i ${SED_PARAM} 's/- ${SPACK_STACK_DIR}/#- ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/config.yaml
+sed -i ${SED_PARAM} 's/extensions:/#extensions:/g' ./etc/spack/defaults/base/config.yaml
+sed -i ${SED_PARAM} 's/- ${SPACK_STACK_DIR}/#- ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/base/config.yaml
 
-sed -i ${SED_PARAM} 's/#builtin:/builtin:/g' ./etc/spack/defaults/repos.yaml
-sed -i ${SED_PARAM} 's/#  git: https/  git: https/g' ./etc/spack/defaults/repos.yaml
-sed -i ${SED_PARAM} 's/#  branch: /  branch: /g' ./etc/spack/defaults/repos.yaml
-sed -i ${SED_PARAM} 's/builtin: ${SPACK_STACK_DIR}/#builtin: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/repos.yaml
-sed -i ${SED_PARAM} 's/spack_stack: ${SPACK_STACK_DIR}/#spack_stack: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/repos.yaml
+sed -i ${SED_PARAM} 's/#builtin:/builtin:/g' ./etc/spack/defaults/base/repos.yaml
+sed -i ${SED_PARAM} 's/#  git: https/  git: https/g' ./etc/spack/defaults/base/repos.yaml
+sed -i ${SED_PARAM} 's/#  branch: /  branch: /g' ./etc/spack/defaults/base/repos.yaml
+sed -i ${SED_PARAM} 's/builtin: ${SPACK_STACK_DIR}/#builtin: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/base/repos.yaml
+sed -i ${SED_PARAM} 's/spack_stack: ${SPACK_STACK_DIR}/#spack_stack: ${SPACK_STACK_DIR}/g' ./etc/spack/defaults/base/repos.yaml
 
 set +e
 
